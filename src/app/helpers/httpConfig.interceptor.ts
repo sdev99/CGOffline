9591013ef0674d983bbf9e6f9e2f8e43bee9f8fb
Sukhdev Patidar <sukhdev.patidar99@gmail.com>
2021-03-26 16:38:09 +0530

    Updated httpConfig

--- src/app/helpers/httpConfig.interceptor.ts
+++ src/app/helpers/httpConfig.interceptor.ts
@@ -1,100 +1,85 @@
-import {
-    HttpRequest,
-    HttpHandler,
-    HttpEvent,
-    HttpInterceptor,
-    HttpResponse,
-    HttpErrorResponse
-} from '@angular/common/http';
-import {from, Observable, throwError} from 'rxjs';
-import {map, catchError, mergeMap} from 'rxjs/operators';
-import {Injectable} from '@angular/core';
-import {EnumService} from '../services/enum.service';
-import {SharedDataService} from '../services/shared-data.service';
-import {AccountService} from '../services/account.service';
-import {Response} from '../_models/response';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { from, Observable, throwError } from 'rxjs';
+import { map, catchError, mergeMap } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { EnumService } from '../services/enum.service';
+import { SharedDataService } from '../services/shared-data.service';
+import { AccountService } from '../services/account.service';
+import { Response } from '../_models/response';
 
 // WDEyREJVTTFqaEljUlc2U05HMlF1WWxNc3dXS2pVa3RKQ0F5c2drWnpSRU9GZWplTU02UHlKcU92a1FqRFF3dVBiNDhYUEFjdVoxT2grUmVRSjE5dkdzR1ZYWkFHWFdmNHU5T1pzbnk3ald0ckVuTkw1OHFvbEsrMXFvMURCb3E=
 @Injectable()
 export class HttpConfigInterceptor implements HttpInterceptor {
+	constructor(public sharedDataService: SharedDataService, private accountService: AccountService) {}
 
-    constructor(
-        public sharedDataService: SharedDataService,
-        private accountService: AccountService,
-    ) {
-    }
+	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		const accessID = this.sharedDataService.deviceUID;
+		const token = localStorage.getItem(EnumService.LocalStorageKeys.API_TOKEN);
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		// Authentication by setting header with token value
+		if (accessID && token && !request.headers.has('accessID') && !request.headers.has('token')) {
+			request = request.clone({
+				setHeaders: {
+					accessID,
+					token,
+				},
+			});
+		}
 
-        const accessID = this.sharedDataService.deviceUID;
-        const token = localStorage.getItem(EnumService.LocalStorageKeys.API_TOKEN);
+		// if (!request.headers.has('Content-Type')) {
+		//     request = request.clone({
+		//         setHeaders: {
+		//             'content-type': 'application/json'
+		//         }
+		//     });
+		// }
 
-        // Authentication by setting header with token value
-        if (accessID && token && !request.headers.has('accessID') && !request.headers.has('token')) {
-            request = request.clone({
-                setHeaders: {
-                    accessID,
-                    token
-                }
-            });
-        }
+		request = request.clone({
+			headers: request.headers.set('Accept', 'application/json'),
+		});
 
-        // if (!request.headers.has('Content-Type')) {
-        //     request = request.clone({
-        //         setHeaders: {
-        //             'content-type': 'application/json'
-        //         }
-        //     });
-        // }
+		return next.handle(request).pipe(
+			map((event: HttpEvent<any>) => {
+				if (event instanceof HttpResponse) {
+					console.log('event--->>>', event);
+				}
+				return event;
+			}),
+			catchError((err: HttpErrorResponse) => {
+				// if token expired then get new token and resend request again
+				const error: Response = err.error;
 
-        request = request.clone({
-            headers: request.headers.set('Accept', 'application/json')
-        });
+				if (error) {
+					if (error.StatusCode === EnumService.ApiResponseCode.InvalidToken) {
+						return from(this.accountService.getToken()).pipe(
+							mergeMap((res) => {
+								request = request.clone({
+									setHeaders: {
+										accessID,
+										token: res.token,
+									},
+								});
+								return this.intercept(request, next);
+							})
+						);
+					}
+					let errorMessage = '';
+					if (error.ResponseException && error.ResponseException.ValidationErrors && error.ResponseException.ValidationErrors.length > 0) {
+						error.ResponseException.ValidationErrors.map((data) => {
+							errorMessage = errorMessage + data.Field + ' : ' + data.Message + '\n';
+						});
+					}
 
-
-        return next.handle(request).pipe(
-            map((event: HttpEvent<any>) => {
-                if (event instanceof HttpResponse) {
-                    console.log('event--->>>', event);
-                }
-                return event;
-            }),
-            catchError((err: HttpErrorResponse) => {
-                // if token expired then get new token and resend request again
-                const error: Response = err.error;
-
-                if (error) {
-                    if (error.StatusCode === EnumService.ApiResponseCode.InvalidToken) {
-                        return from(this.accountService.getToken()).pipe(mergeMap(res => {
-                            request = request.clone({
-                                setHeaders: {
-                                    accessID,
-                                    token: res.token
-                                }
-                            });
-                            return this.intercept(request, next);
-                        }));
-                    } else if (error.StatusCode === EnumService.ApiResponseCode.InvalidData) {
-                        let errorMessage = '';
-                        error.ResponseException.ValidationErrors.map((data) => {
-                            errorMessage = errorMessage + data.Field + ' : ' + data.Message + '\n';
-                        });
-                        return throwError({
-                            error,
-                            message: errorMessage
-                        });
-                    }
-
-                    return throwError({
-                        error,
-                        message: error?.ResponseException?.ExceptionMessage
-                    });
-                }
-                return throwError({
-                    error: err,
-                    message: err.message
-                });
-
-            }));
-    }
+					return throwError({
+						error,
+						message: errorMessage || error?.ResponseException?.ExceptionMessage || err.message,
+					});
+				}
+				return throwError({
+					error: err,
+					message: err.message,
+				});
+			})
+		);
+	}
 }
--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,238 +1,260 @@
-import {Injectable} from '@angular/core';
-import {BehaviorSubject, Observable, throwError} from 'rxjs';
-import {User} from '../_models';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {ActivatedRoute, Router, RouterOutlet} from '@angular/router';
-import {catchError, map} from 'rxjs/operators';
-import {EnumService} from './enum.service';
-import {environment} from '../../environments/environment';
-import {SharedDataService} from './shared-data.service';
-import {Response} from '../_models/response';
-import {NavController, Platform} from '@ionic/angular';
-import {Profile} from '../_models/profile';
-import {App} from '@capacitor/core';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { User } from '../_models';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { catchError, map } from 'rxjs/operators';
+import { EnumService } from './enum.service';
+import { environment } from '../../environments/environment';
+import { SharedDataService } from './shared-data.service';
+import { Response } from '../_models/response';
+import { NavController, Platform } from '@ionic/angular';
+import { Profile } from '../_models/profile';
+import { App } from '@capacitor/core';
 
 declare global {
-    interface Array<T> {
-        clone(): Array<T>;
-    }
+	interface Array<T> {
+		clone(): Array<T>;
+	}
 }
 
-Array.prototype.clone = function() {
-    return JSON.parse(JSON.stringify(this));
+Array.prototype.clone = function () {
+	return JSON.parse(JSON.stringify(this));
 };
 
 @Injectable({
-    providedIn: 'root'
+	providedIn: 'root',
 })
 export class AccountService {
-    private userSubject: BehaviorSubject<User>;
-    public user: Observable<User>;
+	private userSubject: BehaviorSubject<User>;
+	public user: Observable<User>;
 
-    constructor(
-        private router: Router,
-        public sharedDataService: SharedDataService,
-        private http: HttpClient,
-        private navController: NavController,
-        private platform: Platform,
-    ) {
-        this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem(EnumService.LocalStorageKeys.USER_DATA)));
-        this.user = this.userSubject.asObservable();
+	constructor(private router: Router, public sharedDataService: SharedDataService, private http: HttpClient, private navController: NavController, private platform: Platform) {
+		this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem(EnumService.LocalStorageKeys.USER_DATA)));
+		this.user = this.userSubject.asObservable();
 
-        this.checkForMobileLanguageId();
-    }
+		this.checkForMobileLanguageId();
+	}
 
-    checkForMobileLanguageId() {
-        if (!this.sharedDataService.dedicatedMode) {
-            if (this.userSubject.value?.mobileAppLanguageID) {
-                this.sharedDataService.currentLanguageId = this.userSubject.value?.mobileAppLanguageID;
-            } else if (this.sharedDataService.userProfile && this.sharedDataService.userProfile?.mobileAppLanguageID) {
-                this.sharedDataService.currentLanguageId = this.sharedDataService.userProfile?.mobileAppLanguageID;
-            } else {
-                this.getUserProfile(this.userSubject.value?.userId);
-            }
-        }
-    }
+	checkForMobileLanguageId() {
+		if (!this.sharedDataService.dedicatedMode) {
+			if (this.userSubject.value?.mobileAppLanguageID) {
+				this.sharedDataService.currentLanguageId = this.userSubject.value?.mobileAppLanguageID;
+			} else if (this.sharedDataService.userProfile && this.sharedDataService.userProfile?.mobileAppLanguageID) {
+				this.sharedDataService.currentLanguageId = this.sharedDataService.userProfile?.mobileAppLanguageID;
+			} else {
+				this.getUserProfile(this.userSubject.value?.userId);
+			}
+		}
+	}
 
-    public get userValue(): User {
-        return this.userSubject.value;
-    }
+	public get userValue(): User {
+		return this.userSubject.value;
+	}
 
-    login(email = '', password = '', rememberMe = true) {
-        return this.http.post(`${environment.apiUrl}/${EnumService.ApiMethods.UserSignIn}`, {
-            email,
-            password,
-            rememberMe,
-            deviceID: this.sharedDataService.pushToken,
-            platformName: this.platform.is('ios') ? 'IOS' : 'Android',
-        }).pipe(map((data: Response) => {
-            if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
-                const userInfo: User = data.Result;
-                localStorage.setItem(EnumService.LocalStorageKeys.USER_DATA, JSON.stringify(userInfo));
-                this.userSubject.next(userInfo);
+	login(email = '', password = '', rememberMe = true) {
+		return this.http
+			.post(`${environment.apiUrl}/${EnumService.ApiMethods.UserSignIn}`, {
+				email,
+				password,
+				rememberMe,
+				deviceID: this.sharedDataService.pushToken,
+				platformName: this.platform.is('ios') ? 'IOS' : 'Android',
+			})
+			.pipe(
+				map((data: Response) => {
+					if (data?.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
+						const userInfo: User = data.Result;
+						localStorage.setItem(EnumService.LocalStorageKeys.USER_DATA, JSON.stringify(userInfo));
+						this.userSubject.next(userInfo);
 
-                if (userInfo.mobileAppLanguageID) {
-                    this.sharedDataService.currentLanguageId = userInfo.mobileAppLanguageID;
-                }
-                return data;
-            }
-            return throwError(new Error(data?.ResponseException?.ExceptionMessage));
-        }));
-    }
+						if (userInfo.mobileAppLanguageID) {
+							this.sharedDataService.currentLanguageId = userInfo.mobileAppLanguageID;
+						}
+						return data;
+					}
+					return throwError(new Error(data?.ResponseException?.ExceptionMessage));
+				})
+			);
+	}
 
-    checkMobileSessionExpirationSetting(companyID) {
-        return this.http.get(`${environment.apiUrl}/${EnumService.ApiMethods.GetCompanyMobileSessionDetails}/${companyID}`).pipe(map((data: Response) => {
-            if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
-                if (data.Result?.isMobileSessionExpiration) {
-                    const user = this.userValue;
-                    user.isMobileSessionExpiration = true;
-                    localStorage.setItem(EnumService.LocalStorageKeys.USER_DATA, JSON.stringify(user));
-                    this.userSubject.next(user);
-                }
+	checkMobileSessionExpirationSetting(companyID) {
+		return this.http.get(`${environment.apiUrl}/${EnumService.ApiMethods.GetCompanyMobileSessionDetails}/${companyID}`).pipe(
+			map((data: Response) => {
+				if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
+					if (data.Result?.isMobileSessionExpiration) {
+						const user = this.userValue;
+						user.isMobileSessionExpiration = true;
+						localStorage.setItem(EnumService.LocalStorageKeys.USER_DATA, JSON.stringify(user));
+						this.userSubject.next(user);
+					}
 
-                return data;
-            }
-            return throwError(new Error(data?.ResponseException?.ExceptionMessage));
-        }));
-    }
+					return data;
+				}
+				return throwError(new Error(data?.ResponseException?.ExceptionMessage));
+			})
+		);
+	}
 
-    getUserProfile(userId) {
-        return this.http.get(`${environment.apiUrl}/${EnumService.ApiMethods.GetUserProfileById}/${userId}`).pipe(map((data: Response) => {
-            if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
-                const userProfile: Profile = data.Result;
-                localStorage.setItem(EnumService.LocalStorageKeys.USER_PROFILE, JSON.stringify(userProfile));
-                this.sharedDataService.userProfile = userProfile;
-                this.sharedDataService.currentLanguageId = userProfile?.mobileAppLanguageID;
-                return userProfile;
-            }
+	getUserProfile(userId) {
+		return this.http.get(`${environment.apiUrl}/${EnumService.ApiMethods.GetUserProfileById}/${userId}`).pipe(
+			map((data: Response) => {
+				if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
+					const userProfile: Profile = data.Result;
+					localStorage.setItem(EnumService.LocalStorageKeys.USER_PROFILE, JSON.stringify(userProfile));
+					this.sharedDataService.userProfile = userProfile;
+					this.sharedDataService.currentLanguageId = userProfile?.mobileAppLanguageID;
+					return userProfile;
+				}
 
-            return null;
-        }));
-    }
+				return null;
+			})
+		);
+	}
 
-    getDeviceDetails(deviceUID) {
-        return this.http.get(`${environment.apiUrl}/${EnumService.ApiMethods.GetDeviceDetails}/${deviceUID}`).pipe(map((data: Response) => {
-            return data;
-        }));
-    }
+	getDeviceDetails(deviceUID) {
+		return this.http.get(`${environment.apiUrl}/${EnumService.ApiMethods.GetDeviceDetails}/${deviceUID}`).pipe(
+			map((data: Response) => {
+				return data;
+			})
+		);
+	}
 
-    activateDevice() {
-        const deviceDetailId = this.sharedDataService.dedicatedModeDeviceDetailData.deviceID;
-        return this.http.post(`${environment.apiUrl}/${EnumService.ApiMethods.ActivateDevice}?id=${deviceDetailId}`, {});
-    }
+	activateDevice() {
+		const deviceDetailId = this.sharedDataService.dedicatedModeDeviceDetailData.deviceID;
+		return this.http.post(`${environment.apiUrl}/${EnumService.ApiMethods.ActivateDevice}?id=${deviceDetailId}`, {});
+	}
 
-    getAccessKey() {
-        return this.http.get(`${environment.apiUrl}/${EnumService.ApiMethods.GetAccessKey}`).pipe(map((data: Response) => {
-            if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
-                const accessKey = data.Result;
-                localStorage.setItem(EnumService.LocalStorageKeys.API_ACCESS_KEY, accessKey);
-                return {accessKey};
-            }
+	getAccessKey() {
+		return this.http.get(`${environment.apiUrl}/${EnumService.ApiMethods.GetAccessKey}`).pipe(
+			map((data: Response) => {
+				if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
+					const accessKey = data.Result;
+					localStorage.setItem(EnumService.LocalStorageKeys.API_ACCESS_KEY, accessKey);
+					return { accessKey };
+				}
 
-            return {
-                accessKey: null
-            };
-        }));
-    }
+				return {
+					accessKey: null,
+				};
+			})
+		);
+	}
 
-    getToken() {
-        return this.http.get(`${environment.apiUrl}/${EnumService.ApiMethods.GetToken}`, {
-            headers: new HttpHeaders({
-                accessID: this.sharedDataService.deviceUID,
-                accessKey: localStorage.getItem(EnumService.LocalStorageKeys.API_ACCESS_KEY),
-            })
-        }).pipe(map((data: Response) => {
-            if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
-                const token = data.Result;
-                localStorage.setItem(EnumService.LocalStorageKeys.API_TOKEN, token);
-                return {token};
-            }
+	getToken() {
+		return this.http
+			.get(`${environment.apiUrl}/${EnumService.ApiMethods.GetToken}`, {
+				headers: new HttpHeaders({
+					accessID: this.sharedDataService.deviceUID,
+					accessKey: localStorage.getItem(EnumService.LocalStorageKeys.API_ACCESS_KEY),
+				}),
+			})
+			.pipe(
+				map((data: Response) => {
+					if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
+						const token = data.Result;
+						localStorage.setItem(EnumService.LocalStorageKeys.API_TOKEN, token);
+						return { token };
+					}
 
-            return {
-                token: null
-            };
-        }));
-    }
+					return {
+						token: null,
+					};
+				})
+			);
+	}
 
-    newAccountSetup(body) {
-        return this.http.post(`${environment.apiUrl}/${EnumService.ApiMethods.AccountSetup}`, {
-            deviceID: this.sharedDataService.pushToken,
-            platformName: this.platform.is('ios') ? 'IOS' : 'Android',
-            ...body
-        }).pipe(map((data: Response) => {
-            if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
-                const userInfo = data.Result;
-                localStorage.setItem(EnumService.LocalStorageKeys.USER_DATA, JSON.stringify(userInfo));
-                this.userSubject.next(userInfo);
-                return data;
-            }
-            return throwError(new Error(data?.ResponseException?.ExceptionMessage));
-        }));
-    }
+	newAccountSetup(body) {
+		return this.http
+			.post(`${environment.apiUrl}/${EnumService.ApiMethods.AccountSetup}`, {
+				deviceID: this.sharedDataService.pushToken,
+				platformName: this.platform.is('ios') ? 'IOS' : 'Android',
+				...body,
+			})
+			.pipe(
+				map((data: Response) => {
+					if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
+						const userInfo = data.Result;
+						localStorage.setItem(EnumService.LocalStorageKeys.USER_DATA, JSON.stringify(userInfo));
+						this.userSubject.next(userInfo);
+						return data;
+					}
+					return throwError(new Error(data?.ResponseException?.ExceptionMessage));
+				})
+			);
+	}
 
-    forgotpassword(email) {
-        return this.http.post(`${environment.apiUrl}/${EnumService.ApiMethods.ForgotPassword}`, {email}).pipe(map((data: Response) => {
-            if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
-                const message = data.Message;
-                return message;
-            }
-            return null;
-        }));
-    }
+	forgotpassword(email) {
+		return this.http.post(`${environment.apiUrl}/${EnumService.ApiMethods.ForgotPassword}`, { email }).pipe(
+			map((data: Response) => {
+				if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
+					const message = data.Message;
+					return message;
+				}
+				return null;
+			})
+		);
+	}
 
+	resetpassword(body) {
+		return this.http
+			.post(`${environment.apiUrl}/${EnumService.ApiMethods.ResetPassword}`, {
+				deviceID: this.sharedDataService.pushToken,
+				platformName: this.platform.is('ios') ? 'IOS' : 'Android',
+				...body,
+			})
+			.pipe(
+				map((data: Response) => {
+					if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
+						const message = data.Message;
+						return message;
+					}
+					return null;
+				})
+			);
+	}
 
-    resetpassword(body) {
-        return this.http.post(`${environment.apiUrl}/${EnumService.ApiMethods.ResetPassword}`, {
-            deviceID: this.sharedDataService.pushToken,
-            platformName: this.platform.is('ios') ? 'IOS' : 'Android',
-            ...body
-        }).pipe(map((data: Response) => {
-            if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
-                const message = data.Message;
-                return message;
-            }
-            return null;
-        }));
-    }
+	changePassword(body) {
+		return this.http.post(`${environment.apiUrl}/${EnumService.ApiMethods.ChangePassword}`, body).pipe(
+			map((data: Response) => {
+				if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
+					const message = data.Message;
+					return message;
+				}
+				return data.Message;
+			})
+		);
+	}
 
-    changePassword(body) {
-        return this.http.post(`${environment.apiUrl}/${EnumService.ApiMethods.ChangePassword}`, body).pipe(map((data: Response) => {
-            if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
-                const message = data.Message;
-                return message;
-            }
-            return data.Message;
-        }));
-    }
+	updateProfile(body) {
+		return this.http.put(`${environment.apiUrl}/${EnumService.ApiMethods.UpdateUserProfile}`, body).pipe(
+			map((data: Response) => {
+				return data;
+			})
+		);
+	}
 
-    updateProfile(body) {
-        return this.http.put(`${environment.apiUrl}/${EnumService.ApiMethods.UpdateUserProfile}`, body).pipe(map((data: Response) => {
-            return data;
-        }));
-    }
+	logout(userId, navigateToLogin = true) {
+		const option: any = {
+			body: {
+				deviceID: this.sharedDataService.deviceUID,
+			},
+		};
 
-
-    logout(userId, navigateToLogin = true) {
-        const option: any = {
-            body: {
-                deviceID: this.sharedDataService.deviceUID
-            }
-        };
-
-
-        return this.http.delete(`${environment.apiUrl}/${EnumService.ApiMethods.UserDeviceDelete}/${userId}`, option).pipe(map((data: any) => {
-            if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
-                // remove user from local storage and set current user to null
-                localStorage.removeItem(EnumService.LocalStorageKeys.USER_DATA);
-                localStorage.removeItem(EnumService.LocalStorageKeys.USER_PROFILE);
-                this.userSubject.next(null);
-                if (navigateToLogin) {
-                    this.navController.navigateBack(['login'], {replaceUrl: true});
-                    window.location.reload();
-                }
-            }
-            return data;
-        }));
-    }
-
+		return this.http.delete(`${environment.apiUrl}/${EnumService.ApiMethods.UserDeviceDelete}/${userId}`, option).pipe(
+			map((data: any) => {
+				if (data.StatusCode === EnumService.ApiResponseCode.RequestSuccessful) {
+					// remove user from local storage and set current user to null
+					localStorage.removeItem(EnumService.LocalStorageKeys.USER_DATA);
+					localStorage.removeItem(EnumService.LocalStorageKeys.USER_PROFILE);
+					this.userSubject.next(null);
+					if (navigateToLogin) {
+						this.navController.navigateBack(['login'], { replaceUrl: true });
+						window.location.reload();
+					}
+				}
+				return data;
+			})
+		);
+	}
 }
