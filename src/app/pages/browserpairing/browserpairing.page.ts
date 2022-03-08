208cc46a9a86b32691ba9b82f428500de0012ce0
Sukhdev Patidar <sukhdev.patidar99@gmail.com>
2022-03-08 23:10:56 +0530

    Browser pairing design updated

--- src/app/pages/browserpairing/browserpairing.page.ts
+++ src/app/pages/browserpairing/browserpairing.page.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UtilService } from "src/app/services/util.service";
 
 @Component({
-  selector: 'app-browserpairing',
-  templateUrl: './browserpairing.page.html',
-  styleUrls: ['./browserpairing.page.scss'],
+  selector: "app-browserpairing",
+  templateUrl: "./browserpairing.page.html",
+  styleUrls: ["./browserpairing.page.scss"],
 })
 export class BrowserpairingPage implements OnInit {
+  pairingCode = UtilService.uniqueIdForWebApp();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
